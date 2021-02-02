9f2464be5dd4442dcc10de4548b4cc61712ff28e
jaay-austin <52963723+jaay-austin@users.noreply.github.com>
2021-02-02 13:59:20 -0800

    Minor commit to the plain UI without any firebase, MongoDB etc

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import './styles/main.scss'
 import Home from './routes/Home';
 import ProfilePage from './routes/ProfilePage';
 
-// ReactDOM.render(
-//   <Home />,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
-  <ProfilePage />,
+  <Home />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <ProfilePage />,
+//   document.getElementById('root')
+// );
